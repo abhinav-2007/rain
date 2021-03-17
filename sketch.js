30548ecbff824c4cd51fd773dbde3da9742b09b8
abhinav-2007 <74411463+abhinav-2007@users.noreply.github.com>
2021-03-17 18:16:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var t1,t2,t3,t4;
+var engine,world,umbrella1;
+var drop1=[];
+var maxDrops=100;
+function preload()
+{
+	t1=loadImage("images/thunderbolt/1.png")
+	t2=loadImage("images/thunderbolt/2.png")
+	t3=loadImage("images/thunderbolt/3.png")
+	t4=loadImage("images/thunderbolt/4.png")
+
+	
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	umbrella1=new umbrella(250,500);
+	if(frameCount%120===0){
+		for(var i=0;i<maxDrops;i++){
+			drop1.push(new rain (random(0,800),random(0,500)));
+		}
+	}
+
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  rand=Math.round(random(1,4));
+  if(frameCount%60===0){
+	var thunder=createSprite(random(20,350),random(10,40),10,10);
+	  switch(rand){
+		  case 1:thunder.addImage(t1);
+		  break;
+		  case 2:thunder.addImage(t2);
+		  break;
+		  case 3:thunder.addImage(t3);
+		  break;
+		  case 4:thunder.addImage(t4);
+		  break;
+		  default:break;
+	  }
+	  thunder.lifetime=50;
+	  thunder.scale =0.6;
+      
+  }
+  
+
+  umbrella1.display();
+  for(var i=0;i<maxDrops;i++){
+	  drop1[i].showdrop();
+	  drop1[i].update();
+
+  }
+  
+  drawSprites();
+ 
+}
+
+
+
